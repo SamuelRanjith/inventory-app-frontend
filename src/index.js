bee1acb069a9dada656d2b2fb9d949a078b3e9bc
SamuelRanjith <samuelranjith11@gmail.com>
2022-07-18 18:43:31 +0530

    React App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './Components/App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import CreateProduct from './Components/products/CreateProduct';
+import SingleProducts from './Components/products/SingleProduct';
+import EditProduct from './Components/products/EditProduct';
+import Login from './Components/users/Login';
+import Signup from './Components/users/Signup';
+import GetSearching from './Components/products/GetSearching';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <Router>
+    <Switch>
+    <Route exact path="/login" render= {(props)=>{ return <Login {...props}/>}} />
+    <Route exact path="/register" render= {(props)=>{ return <Signup {...props}/>}} />
+    <Route exact path='/' render= {(props)=>{ return <App {...props}/>}}/>
+    <Route exact path="/add" component= {(props)=>{ return <CreateProduct {...props}/>}} />
+    <Route exact path="/products/:id" component= {(props)=>{ return <SingleProducts {...props}/>}} />
+    <Route exact path="/products/edit/:id" component= {(props)=>{ return <EditProduct {...props}/>}} />
+    <Route exact path="/getsearching/:name" component= {(props)=>{ return <GetSearching {...props}/>}}/>
+  
+    </Switch>
+    </Router>
+, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
